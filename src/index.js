d37116ebab1b45d5d0493134476fafade5565a53
Kalin Jack <kalinj1997@gmail.com>
2021-03-19 16:59:16 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const taskForm = document.getElementById("task-form");
+  const taskMain = document.getElementById("task-main");
+  const taskDescription = document.getElementById("task-description");
+  
+  const newTaskUl = document.getElementById("tasks");
+
+  newtaskform.addEventListener("submit",createNewTask);
+
+
 });
+
+
+const createNewTask = event => {
+  event.preventDefault();
+    const newTaskDescription = document.getElementById("new-task-description");
+    const newTask = document.createElement("li");
+    newTask.innerText = newTaskDescription.value;
+  
+    appendNewTask(newTask);
+    event.target.reset();
+};
+
+
+
+
+
+  
+
+
+
+
+
