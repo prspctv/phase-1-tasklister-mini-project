b65a4ea50e7d3d53d11bcff67cd03eea62daccd5
Isabella <wileyisabella@gmail.com>
2021-03-20 22:09:41 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -1,30 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
   
-  const taskForm = document.getElementById("task-form");
-  const taskMain = document.getElementById("task-main");
-  const taskDescription = document.getElementById("task-description");
+  const taskForm = document.getElementById("create-task-form");
+  const taskMain = document.getElementById("main-content");
+  //const taskDescription = document.getElementById("task-description");
   
   const newTaskUl = document.getElementById("tasks");
 
-  newtaskform.addEventListener("submit",createNewTask);
+  taskForm.addEventListener("submit",createNewTask);
 
+
+
+
+  function createNewTask(event){
+   event.preventDefault()
+   let newTask = document.createElement("li")
+   let description = document.querySelector("#new-task-description").value
+   newTask.innerText = description
+   newTaskUl.append(newTask)
+   console.log(newTaskUl)
+}
 
 });
 
-
-const createNewTask = event => {
-  event.preventDefault();
-    const newTaskDescription = document.getElementById("new-task-description");
-    const newTask = document.createElement("li");
-    newTask.innerText = newTaskDescription.value;
+// const createNewTask = event => {
+//   event.preventDefault();
+//     const newTaskDescription = document.getElementById("new-task-description");
+//     const newTask = document.createElement("li");
+//     newTask.innerText = newTaskDescription.value;
   
-    appendNewTask(newTask);
-    event.target.reset();
-};
+//     appendNewTask(newTask);
+//     event.target.reset();
+// };
 
 
 
-
+/* add event listener
+preventDefault
+create li
+assign text to li
+add li to list
+*/
 
   
 
